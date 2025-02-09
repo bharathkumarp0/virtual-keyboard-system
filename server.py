49029de1ada47f91e6726d2b7a670b2669998f50
bharathkumarp0 <bharathkumarp0058@gmail.com>
2025-02-09 22:50:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -18,12 +18,14 @@ def start_typing():
         if process is None:
             python_executable = sys.executable
             process = subprocess.Popen([python_executable, "main.py"], cwd=os.getcwd(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print("✅ main.py started successfully")
             return jsonify({"message": "Virtual Keyboard Started"}), 200
         else:
+            print("⚠️ Virtual Keyboard is already running.")
             return jsonify({"message": "Virtual Keyboard is already running"}), 200
     except Exception as e:
+        print("❌ Error starting main.py:", str(e))
         return jsonify({"error": str(e)}), 500
-
 @app.route('/stop-typing', methods=['GET'])
 def stop_typing():
     global process
@@ -37,5 +39,5 @@ def stop_typing():
     return jsonify({"error": "No active process found"}), 400
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Get Render-assigned port
+    port = int(os.environ.get("PORT", 5000))  # Use Render-assigned port
     app.run(host='0.0.0.0', port=port, debug=True)
